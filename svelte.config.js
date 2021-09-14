ee733edc4fe28f4b4fc0153350632996237ed975
Timo <38291523+lovetodream@users.noreply.github.com>
2021-09-14 22:44:58 +0200

    ✨ Add path alias for src

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,26 @@
 import preprocess from 'svelte-preprocess';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({
-        "postcss": true
-    })],
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			resolve: {
+				alias: {
+					'@': path.resolve('./src')
+				}
+			}
+		}
 	}
 };
 

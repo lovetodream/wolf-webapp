a43c2551b7c77c50656f71da1f99a1708b6ce257
Timo <38291523+lovetodream@users.noreply.github.com>
2021-10-14 21:45:48 +0200

    ✨ Add help/additional resources page

---
+++ src/routes/dashboard/[id]/settings/help.svelte
@@ -0,0 +1,107 @@
+<div class="max-w-3xl mx-auto py-10 px-4 sm:px-6 lg:py-12 lg:px-8">
+  <h1 class="text-3xl font-extrabold text-gray-900">Additional Resources</h1>
+
+  <div class="mt-6 space-y-8 divide-y divide-y-gray-200 flow-root">
+    <ul role="list" class="-mb-8">
+      <li>
+        <div class="relative pb-8">
+          <div class="relative flex space-x-3">
+            <div>
+              <span class="h-8 w-8 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-gray-50">
+                <i class="!h-4 !w-4 text-white far fa-book"></i>
+              </span>
+            </div>
+            <div class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+              <div>
+                <p class="text-sm text-gray-500">Check out the <a href="." class="font-medium text-gray-900 cursor-wait">Documentation</a></p>
+              </div>
+              <div class="text-right text-sm whitespace-nowrap text-gray-500">
+                <span>available soon</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+  
+      <li>
+        <div class="relative pb-8">
+          <div class="relative flex space-x-3">
+            <div>
+              <span class="h-8 w-8 rounded-full bg-blue-500 flex items-center justify-center ring-8 ring-gray-50">
+                <i class="!h-4 !w-4 text-white far fa-code-merge"></i>
+              </span>
+            </div>
+            <div class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+              <div>
+                <p class="text-sm text-gray-500">Contribute on <a href="https://github.com/lovetodream/wolf-webapp" class="font-medium text-gray-900">Github</a></p>
+              </div>
+              <div class="text-right text-sm whitespace-nowrap text-gray-500">
+                <time datetime="2020-09-30">1 contributor</time>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+  
+      <li>
+        <div class="relative pb-8">
+          <div class="relative flex space-x-3">
+            <div>
+              <span class="h-8 w-8 rounded-full bg-red-500 flex items-center justify-center ring-8 ring-gray-50">
+                <i class="!h-4 !w-4 text-white fas fa-heart"></i>
+              </span>
+            </div>
+            <div class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+              <div>
+                <p class="text-sm text-gray-500">Donate to the projects creator <a href="." class="font-medium text-gray-900">Timo Zacherl</a></p>
+              </div>
+              <div class="text-right text-sm whitespace-nowrap text-gray-500">
+                <time datetime="2020-09-30">0.00 $ per month</time>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+  
+      <li>
+        <div class="relative pb-8">
+          <div class="relative flex space-x-3">
+            <div>
+              <span class="h-8 w-8 rounded-full bg-yellow-500 flex items-center justify-center ring-8 ring-gray-50">
+                <i class="!h-4 !w-4 text-white far fa-exclamation-circle"></i>
+              </span>
+            </div>
+            <div class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+              <div>
+                <p class="text-sm text-gray-500">Report an <a href="https://github.com/lovetodream/wolf-backend/issues/new?assignees=&labels=&template=bug_report.md&title=" class="font-medium text-gray-900">Issue</a></p>
+              </div>
+              <div class="text-right text-sm whitespace-nowrap text-gray-500">
+                <time datetime="2020-09-30">9 open</time>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+  
+      <li>
+        <div class="relative pb-8">
+          <div class="relative flex space-x-3">
+            <div>
+              <span class="h-8 w-8 rounded-full bg-green-500 flex items-center justify-center ring-8 ring-gray-50">
+                <i class="!h-5 !w-5 text-white far fa-lightbulb-on"></i>
+              </span>
+            </div>
+            <div class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+              <div>
+                <p class="text-sm text-gray-500">Request a <a href="https://github.com/lovetodream/wolf-backend/issues/new?assignees=&labels=&template=feature_request.md&title=" class="font-medium text-gray-900">Feature</a></p>
+              </div>
+              <div class="text-right text-sm whitespace-nowrap text-gray-500">
+                <time datetime="2020-09-30">1 open</time>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>

666224ed632770fa36cf1d9d3d2a1443279fdab3
Timo <38291523+lovetodream@users.noreply.github.com>
2021-10-14 21:43:52 +0200

    ✨ Add dashboard home (placeholder)

---
+++ src/routes/dashboard/[id]/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { gqlGetSingleProject } from '@/generated/graphql';
+import { currentProjectID } from '@/stores';
+import { get } from 'svelte/store';
+
+$: query = gqlGetSingleProject({ variables: { id: get(currentProjectID) } })
+</script>
+
+{ $currentProjectID }
+
+{ JSON.stringify($query.data.project) }
